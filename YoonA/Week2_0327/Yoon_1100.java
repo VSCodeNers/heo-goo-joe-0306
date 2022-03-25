a5c58e4a19c3e220768d54a1aaa8f5b8a2b03f0e
unknown <gjdbsdk18@icloud.com>
2022-03-25 18:07:22 +0900

    2주차 과제

---
+++ YoonA/Week2_0327/Yoon_1100.java
@@ -0,0 +1,33 @@
+public class Yoon_1100 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		char chess[][] = new char[8][8];
+		
+		int count = 0;
+		
+		for (int i = 0; i < chess.length; i++) {
+			String s = sc.next();
+
+			for (int j = 0; j < chess.length; j++) {
+				chess[i][j] = s.charAt(j);
+			}
+		}
+		
+		for (int i = 0; i < chess.length; i++) {
+			for (int j = 0; j < chess.length; j++) {
+				if (i % 2 == 0 && j % 2 == 0) {     //짝행 짝열
+					if (chess[i][j] == 'F')
+						count++;
+				}
+
+				if (i % 2 != 0 && j % 2 != 0) {     //홀행 홀열
+					if (chess[i][j] == 'F')
+						count++;
+				}
+			}
+		}
+
+		System.out.println(count);
+		sc.close();
+	}
+}
