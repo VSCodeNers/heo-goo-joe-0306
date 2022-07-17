9d5e96d559567adaf96af02cbaea98d2c8f33fd4
yoona1110 <gjdbsdk18@icloud.com>
2022-07-17 13:41:00 +0900

    10주차

--- YoonA/Week10_0717/Yoon_1748.java
+++ YoonA/Week10_0717/Yoon_1748.java
@@ -10,25 +10,13 @@ public class Yoon_1748 {
         int res = 0;
 
         for (int i = 1; i <= N; i*=10) {
-            int end = i * 10 - 1;
+            int end = i * 10 - 1;   //자릿수 끝을 맞춰줌
 
-            if (end > N) end = N;
+            if (end > N) end = N;   //end가 N보다 크면 안되므로
 
             res += (end - i + 1) * len;
             len++;
         }
-
-        // while (start <= N) {
-        //     int end = start * 10 - 1;   //각 자릿수
-
-        //     if (end > N) 
-        //         end = N;    //end이 N보다 크면 안되니까 N으로 맞춰줌
-
-        //     res += (end - start + 1) * len;
-        //     start *= 10;    //자릿수 계산 끝냈으면 다음 자릿수 계산해야하므로
-        //     len += 1;   //자릿수 늘어날수록 길이도 늘어나니까
-        // }
-
         System.out.println(res);
     }
 }
