e85d60b4b9f55db7984dfe66024fb443cfe7280c
jung0115 <jungim7490@gmail.com>
2022-08-15 23:04:37 +0900

    14주차 백준

---
+++ JeongMi/week14_0821/BeakJoon_15656.java
@@ -0,0 +1,38 @@
+// 백준 - N과 M 7(15656)
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class BeakJoon_15656 {
+    static int N, M;
+    static int[] num;
+    static StringBuilder result = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        num = new int[N];
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num); // 오름차순 정렬
+        NandM7(0, "");
+
+        System.out.print(result);
+    }
+    static void NandM7(int cnt, String sequence) {
+        if(cnt == M) { // 길이가 M인 수열이 만들어진 경우
+            result.append(sequence).append("\n");
+            return;
+        }
+        for(int i = 0; i < N; i++) {
+            NandM7(cnt + 1, sequence + num[i] + " ");
+        }
+    }
+}
