a32ef749947f0586e5adff6cfa263d9a0679aeda
yoona1110 <gjdbsdk18@icloud.com>
2022-11-02 02:16:36 +0900

    19주차 수정

--- YoonA/Week19_1106/Yoon_BJ14501.java
+++ YoonA/Week19_1106/Yoon_BJ14501.java
@@ -12,28 +12,34 @@ public class Yoon_BJ14501 {
         T = new int[N+1];
         P = new int[N+1];       
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             T[i] = Integer.parseInt(st.nextToken());
             P[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             DP(i, T[i], P[i], 0);
         }
+
         System.out.println(max);
     }
 
-    public static void DP(int day, int time, int price, int total) {
-        if (day + time >= N + 1) {
-            if (day + time == N + 1) {
-                total += price;
-            }
-            max = Math.max(total, max);
+    public static void DP(int day, int period, int profit, int total) {
+        /* 현재 일(day)에 상담 걸리는 기간(period) 더해서 n+1일째가 되면 상담 불가이므로
+           그대로 Math.max로 최대 수익(max) 구해서 출력
+           근데 딱 퇴사날이라면 수익 합계에 그 상담 수익까지 합함
+        */ 
+        if (day + period >= N + 1) {
+            if (day + period == N + 1) { 
+                total += profit;           
+            }       
+            max = Math.max(total, max);    
             return;
         }
-        for (int i = day + time; i <= N; i++) {
-            DP(i, T[i], P[i], total + price);
+        /* 지난 기간부터 시작해서 퇴사날 될 때까지 계속 반복함 */
+        for (int i = day + period; i <= N; i++) {
+            DP(i, T[i], P[i], total + profit);
         }
     }
 }
--- YoonA/Week19_1106/Yoon_BJ1759.java
+++ YoonA/Week19_1106/Yoon_BJ1759.java
@@ -1,3 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Yoon_BJ1759 {
-    
+    public static int L, C;
+    public static char ch[];
+    public static boolean check[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        L = Integer.parseInt(br.readLine());
+        C = Integer.parseInt(br.readLine());
+        ch = new char[C];
+        check = new boolean[C];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            ch[i] = st.nextToken().charAt(0);
+        }
+        Arrays.sort(ch);
+
+        DP(0, 0);
+    }
+
+    public static void DP(int start, int count) {
+        int vowel = 0;          // 모음
+        int consonant = 0;      // 자음
+        String password ="";
+
+        for (int i = start; i < C; i++) {
+            check[i] = true;
+            DP(i+1, start+1);
+            check[i] = false;
+        }
+        if (count == C) {
+            if (vowel >= 1 && consonant >= 2) {
+                System.out.println(password);
+            }
+
+            for (int i = 0; i < C; i++) {
+                if (check[i] == true) {
+                    password += ch[i];
+
+                    if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u') {
+                        vowel++;
+                    }
+                    else {
+                        consonant++;
+                    }
+                }
+            }
+        }
+    }
 }
