5c05fbd16639f2e233a4c110ea9d87dff07a30b2
jung0115 <jungim7490@gmail.com>
2022-11-13 05:00:22 +0900

    20주차 백준

--- JeongMi/week20_1113/BeakJoon_15661.java
+++ JeongMi/week20_1113/BeakJoon_15661.java
@@ -1,10 +1,10 @@
-// 백준 - 스타트와 링크(14889)
+// 백준 - 링크와 스타트(15661)
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class BeakJoon_14889 {
+public class BeakJoon_15661 {
   static int[][] skillScore;
   static int min = 1000000; //능력치 차이 최소값
   public static void main(String[] args) throws IOException {
@@ -17,14 +17,17 @@ public class BeakJoon_14889 {
         skillScore[i][j] = Integer.parseInt(st.nextToken());
       }
     }
-
-    int[] start = new int[N/2];
-    startAndLink(N, start, 0);
+    
+    // 스타트 팀의 인원수가 1명부터 N-1명인 경우까지
+    for(int i = 1; i < N; i++) {
+      int[] start = new int[i];
+      startAndLink(N, start, 0, i);
+    }
     System.out.print(min);
   }
-  static void startAndLink(int N, int[] start, int cnt) {
-    if(cnt == N/2) {
-      int scoreSub = calculateScore(N, start);
+  static void startAndLink(int N, int[] start, int cnt, int size) {
+    if(cnt == size) {
+      int scoreSub = calculateScore(N, start, size);
       if(scoreSub < min){
         min = scoreSub;
       }
@@ -35,25 +38,25 @@ public class BeakJoon_14889 {
     if(cnt != 0) startI = start[cnt-1]+1;
     for(int i = startI; i < N; i++) {
       start[cnt] = i;
-      startAndLink(N, start, cnt+1);
+      startAndLink(N, start, cnt+1, size);
     }
   }
-  static int calculateScore(int N, int[] start) {
-    int[] link = new int[N/2];
+  static int calculateScore(int N, int[] start, int size) {
+    int[] link = new int[N-size];
     int cntS = 0, cntL = 0;
     for(int i = 0; i < N; i++) {
-      if(cntS == N/2 || start[cntS] != i) {
+      if(cntS == size || start[cntS] != i) {
         link[cntL] = i;
         cntL++;
-        if(cntL == N/2) break;
+        if(cntL == (N-size)) break;
       }
       else cntS++;
     }
 
     //스타트 팀의 능력치 계산
     int startScore = 0;
-    for(int i = 0; i < N/2; i++) {
-      for(int j = i+1; j < N/2; j++) {
+    for(int i = 0; i < size; i++) {
+      for(int j = i+1; j < size; j++) {
         startScore += skillScore[start[i]][start[j]];
         startScore += skillScore[start[j]][start[i]];
       }
@@ -61,8 +64,8 @@ public class BeakJoon_14889 {
 
     //링크 팀의 능력치 계산
     int linkScore = 0;
-    for(int i = 0; i < N/2; i++) {
-      for(int j = i+1; j < N/2; j++) {
+    for(int i = 0; i < (N-size); i++) {
+      for(int j = i+1; j < (N-size); j++) {
         linkScore += skillScore[link[i]][link[j]];
         linkScore += skillScore[link[j]][link[i]];
       }
