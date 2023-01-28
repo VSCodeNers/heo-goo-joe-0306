f06795c0e0cfad1734af564e923c25eba880af1d
yoona1110 <gjdbsdk18@icloud.com>
2023-01-29 01:52:27 +0900

    프로그래머스 완료

--- YoonA/Week24_0129/Yoon_pro16.java
+++ YoonA/Week24_0129/Yoon_pro16.java
@@ -1,39 +1,49 @@
-import java.util.*;
+import java.util.Arrays;
 
-public class Yoon_pro16 {
-
+class Solution {
     /* lost: 체육복을 도난당한 학생들 
        reserve: 체육복을 가져온 학생들 */
     public int solution(int n, int[] lost, int[] reserve) {
         // 체육복 갖고 온 학생 수 (전체 학생 - 체육복 잃어버린 학생)
-        int possible = n - lost.length; 
+        int possible = 0;
+        possible = n - lost.length; 
         
-        // 오름차순 정렬을 안해두니 실패 케이스 있길래 해둠
+        boolean rc[] = new boolean[n+1];
+        boolean lc[] = new boolean[n+1];
+        
         Arrays.sort(lost);
-        Arrays.sort(reserve);
         
-        // 여분의 체육복이 있어서 빌려주는게 가능한 경우
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] -1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-                    possible++;
-                    reserve[j] = -1; // 또 빌려줄 순 없으니까 배열에 포함 안되도록
-                    break;
-                }
-            }
+        // reserve[] 보고 여분 체육복이 있으면 rc[]에 true
+        for (int i = 0; i < reserve.length; i++) {
+            rc[reserve[i]] = true;
         }
         
-        // 체육복을 도난 당한 경우
         for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    possible++;
-                    lost[i] = -1;       // 또 빌려줄 순 없으니까 배열에 포함 안되도록
-                    reserve[j] = -1;    // 위랑 같음
-                    break;
-                }
+            /* rc[]가 true면 여분이 있다는 의미니까
+            i번째 학생한테 체육복 빌려주고 reserve true => false로.
+            수업 가능한 학생이 증가하므로 +1 */
+            if (rc[lost[i]]) {
+                rc[lost[i]] = false;
+                possible++;
             }
+            // rc[] = false면 빌려줄 체육복이 없다는 말이니 lc[] = true로 (잃어버렸다는 뜻)
+            else 
+                lc[lost[i]] = true;
         }
+        
+        for(int i = 0; i < lost.length; i++) {
+            // i번째는 체육복이 없지만 앞 번호(i-1) 학생이 체육복이 있는경우
+        	if(lc[lost[i]] && lost[i] - 1 >= 1 && rc[lost[i] - 1]) {
+        		rc[lost[i] - 1] = false;
+        		possible++;
+        	}
+            // i번째는 체육복이 없지만 뒷 번호(i+1) 학생이 체육복이 있는 경우
+            else if(lc[lost[i]] && lost[i] + 1 <= n && rc[lost[i] + 1]) {
+        		rc[lost[i] + 1] = false;
+        		possible++;
+        	}
+        }
+        
         return possible;
     }
 }
