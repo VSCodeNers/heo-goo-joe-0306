35b354b02c37904f8a20f1ac8abb472def37edf4
yoona1110 <gjdbsdk18@icloud.com>
2023-03-17 19:46:13 +0900

    28주차 완료

--- YoonA/Week28_0319/Yoon_BJ7562.java
+++ YoonA/Week28_0319/Yoon_BJ7562.java
@@ -1,51 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Yoon_BJ7562 {
-    static int N, M;
-    static int Maze[][];
-    static boolean check[][];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	static int N;
+	static int startX, startY, endX, endY;
+	static int[][] chess;
+	static boolean[][] check;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int Number = Integer.parseInt(br.readLine());
+		
+		while(Number-- > 0) {
+			// 체스판의 크기
+			N = Integer.parseInt(br.readLine());
+			
+			chess = new int[N][N];
+			check = new boolean[N][N];
+			
+			// 시작점
+			st = new StringTokenizer(br.readLine());
+			startX = Integer.parseInt(st.nextToken());
+			startY = Integer.parseInt(st.nextToken());
+			
+			// 도착점
+			st = new StringTokenizer(br.readLine());
+			endX = Integer.parseInt(st.nextToken());
+			endY = Integer.parseInt(st.nextToken());
+			
+			System.out.println(BFS(startX, startY));
+		}
+	}
+	
+	public static int BFS(int x, int y) {
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(x, y, 0));
+		check[x][y] = true;
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        Maze = new int[N][M];
-        check = new boolean[N][M];
+        int[] mapX = {-1, -2, -2, -1, 1, 2,  2,  1};
+	    int[] mapY = {-2, -1,  1,  2, 2, 1, -1, -2};
+		
+		while(!q.isEmpty()) {
+            // 큐에서 값을 꺼냄. 그거 기준에서 상하좌우 갈 수 있는 곳으로 간다.
+			Node now = q.poll();
+			// 현재 큐의 좌표
+			int nx = now.x;
+			int ny = now.y;
+			
+			// 현재 좌표가 도착지점이라면 이동횟수 출력
+			if(nx == endX && ny == endY) {
+				return now.cnt;
+			}
+			
+			for(int i = 0; i < 8; i++) {
+				// 다음으로 이동해야 할 좌표
+				int xx = nx + mapX[i];
+				int yy = ny + mapY[i];
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                Maze[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-    }
+                // 이동경로가 체스판의 범위를 벗어나지 않았고, 방문하지 않은 경우
+				if(xx >= 0 && yy >= 0 && xx < N && yy < N) {
+                    if (!check[xx][yy]) {
+                        check[xx][yy] = true;
+				        // 현재좌표에 이동횟수 1을 추가하여 큐에 담음
+				        q.offer(new Node(xx, yy, now.cnt + 1));
+                    }
+				}
+			}
+		}
+		return 0;
+	}
 
-    public static void BFS() {
-        Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(x, y));
-        // 나이트가 이동하고자 하는 칸
-        int mapX[] = {-1, -2, -2, -1, 1, 2, 2, 1};
-        int mapY[] = {-2, -1, 1, 2, 2, 1, -1, -2};
-
-        // 여덟 방향 탐색
-        for (int i = 0; i < 8; i++) {
-            int xx = x + mapX[i];
-            int yy = y + mapY[i];
-
-            // 범위를 벗어나지 않은 경우
-            if(xx >= 0 && yy >= 0 && xx < N && yy < M) {
-                // 아직 방문하지 않은 경우
-                if (!check[xx][yy]) {
-                    check[xx][yy] = true;
-                    // 연결된 땅을 모두 방문함
-                    BFS(xx, yy);
-                }
-            }
-        }
-    }
+    // 맵의 좌표와 이동 횟수
+	static class Node {
+		int x, y, cnt;
+		public Node(int x, int y, int cnt) {
+			this.x = x;
+			this.y = y;
+			this.cnt = cnt;
+		}
+	}
 }
