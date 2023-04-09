39a18e59063061bd44e70fa35ec3a20f07e9f480
jung0115 <jungim7490@gmail.com>
2023-04-09 14:27:26 +0900

    28주차

---
+++ JeongMi/week28_0326/Baekjoon_1697.java
@@ -0,0 +1,54 @@
+// 백준 - 숨바꼭질(1697)
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Baekjoon_1697 {
+  static int N, K;
+  static int[] visited = new int[1000001];
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(bf.readLine());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+
+    System.out.print(checkToBFS());
+  }
+
+  static int checkToBFS() {
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(N);
+    visited[N] = 1;
+
+    while(!queue.isEmpty()) {
+      int prePosition = queue.remove();
+
+      // 도착한 경우
+      if(prePosition == K)
+        return visited[prePosition] - 1;
+      
+      // n-1 이동 시, 범위를 벗어나지 않고 방문한 적이 없는 경우
+      if(prePosition - 1 >= 0 && visited[prePosition - 1] == 0) {
+        visited[prePosition - 1] = visited[prePosition] + 1;
+        queue.add(prePosition - 1);
+      }
+
+      // n+1 이동 시, 범위를 벗어나지 않고 방문한 적이 없는 경우
+      if(prePosition + 1 <= 1000000 && visited[prePosition + 1] == 0) {
+        visited[prePosition + 1] = visited[prePosition] + 1;
+        queue.add(prePosition + 1);
+      }
+
+      // 2*n 이동 시, 범위를 벗어나지 않고 방문한 적이 없는 경우
+      if(prePosition * 2 <= 1000000 && visited[prePosition * 2] == 0) {
+        visited[prePosition * 2] = visited[prePosition] + 1;
+        queue.add(prePosition * 2);
+      }
+    }
+
+    return -1;
+  }
+}
---
+++ JeongMi/week28_0326/Baekjoon_1991.java
@@ -0,0 +1,127 @@
+// 백준 - 트리 순회(1991)
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1991 {
+  static int N;
+  static char[][] nodes;
+  static boolean[] visited;
+  static StringBuilder preResult, inResult, postResult;
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(bf.readLine()); // 노드의 개수
+    nodes = new char[N][3];
+
+    for(int i = 0; i < N; i++) {
+      StringTokenizer st = new StringTokenizer(bf.readLine());
+      int index = st.nextToken().charAt(0) - 'A';
+      nodes[index][0] = (char) (index + 'A');
+      nodes[index][1] = st.nextToken().charAt(0);
+      nodes[index][2] = st.nextToken().charAt(0);
+    }
+
+    // 전위 순회
+    preResult = new StringBuilder();
+    visited = new boolean[N];
+    
+    visited[0] = true;
+    preResult.append('A');
+    preorderTraversal(0);
+    
+    System.out.println(preResult);
+
+    // 중위 순회
+    inResult = new StringBuilder();
+    visited = new boolean[N];
+    inorderTraversal(0);
+    System.out.println(inResult);
+
+    // 후위 순회
+    postResult = new StringBuilder();
+    visited = new boolean[N];
+
+    visited[0] = true;
+    postorderTraversal(0);
+    postResult.append('A');
+    
+    System.out.println(postResult);
+  }
+
+  // 전위 순회
+  static void preorderTraversal(int prePosition) {
+    // 왼쪽 자식 노드가 있는 경우
+    if(nodes[prePosition][1] != '.') {
+      int nextPosition = (int) (nodes[prePosition][1] - 'A');
+      // 방문한 적이 없는 경우
+      if(!visited[nextPosition]) {
+        visited[nextPosition] = true;
+        preResult.append(nodes[prePosition][1]);
+        preorderTraversal(nextPosition); // 오른쪽 노드로 탐방
+      }
+    }
+
+    // 오른쪽 자식 노드가 있는 경우
+    if(nodes[prePosition][2] != '.') {
+      int nextPosition = (int) (nodes[prePosition][2] - 'A');
+      // 방문한 적이 없는 경우
+      if(!visited[nextPosition]) {
+        visited[nextPosition] = true;
+        preResult.append(nodes[prePosition][2]);
+        preorderTraversal(nextPosition); // 오른쪽 노드로 탐방
+      }
+    }
+  }
+
+  // 중위 순회
+  static void inorderTraversal(int prePosition) {
+    // 왼쪽 자식 노드가 있는 경우
+    if(nodes[prePosition][1] != '.') {
+      int nextPosition = (int) (nodes[prePosition][1] - 'A');
+      // 방문한 적이 없는 경우
+      if(!visited[nextPosition]) {
+        visited[nextPosition] = true;
+        inorderTraversal(nextPosition); // 오른쪽 노드로 탐방
+      }
+    }
+
+    visited[prePosition] = true;
+    inResult.append(nodes[prePosition][0]);
+
+    // 오른쪽 자식 노드가 있는 경우
+    if(nodes[prePosition][2] != '.') {
+      int nextPosition = (int) (nodes[prePosition][2] - 'A');
+      // 방문한 적이 없는 경우
+      if(!visited[nextPosition]) {
+        visited[nextPosition] = true;
+        inorderTraversal(nextPosition); // 오른쪽 노드로 탐방
+      }
+    }
+  }
+
+  // 후위 순회
+  static void postorderTraversal(int prePosition) {
+    // 왼쪽 자식 노드가 있는 경우
+    if(nodes[prePosition][1] != '.') {
+      int nextPosition = (int) (nodes[prePosition][1] - 'A');
+      // 방문한 적이 없는 경우
+      if(!visited[nextPosition]) {
+        visited[nextPosition] = true;
+        postorderTraversal(nextPosition); // 오른쪽 노드로 탐방
+        postResult.append(nodes[prePosition][1]);
+      }
+    }
+
+    // 오른쪽 자식 노드가 있는 경우
+    if(nodes[prePosition][2] != '.') {
+      int nextPosition = (int) (nodes[prePosition][2] - 'A');
+      // 방문한 적이 없는 경우
+      if(!visited[nextPosition]) {
+        visited[nextPosition] = true;
+        postorderTraversal(nextPosition); // 오른쪽 노드로 탐방
+        postResult.append(nodes[prePosition][2]);
+      }
+    }
+  }
+}
