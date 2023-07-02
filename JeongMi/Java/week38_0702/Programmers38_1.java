86430c4e64f5d5d1611e4aa573ab2ccf2212449c
jung0115 <jungim7490@gmail.com>
2023-07-02 16:45:05 +0900

    코드 수정

--- JeongMi/Java/week38_0702/Programmers38_1.java
+++ JeongMi/Java/week38_0702/Programmers38_1.java
@@ -2,29 +2,21 @@
 class Solution {
   static int index = 0;
   public int[][] solution(int n) {
-      int[][] answer = new int[(int)Math.pow(2, n) - 1][2];
+    int[][] answer = new int[(int)Math.pow(2, n) - 1][2];
 
-hanoiTop(n, 1, 2, 3, answer);
+    hanoiTop(n, 1, 2, 3, answer);
 
-return answer;
+    return answer;
+  }
 
-}
+  static void hanoiTop(int n, int start, int middle, int finish, int[][] answer){
+    if(n == 1)
+      answer[index++] = new int[] {start, finish};
 
-static void hanoiTop(int n, int start, int middle, int finish, int[][] answer){
-
-if(n == 1)
-
-  answer[index++] = new int[] {start, finish};
-
-else{
-
-  hanoiTop(n-1, start, finish, middle, answer);
-
-  answer[index++] = new int[] {start, finish};
-
-  hanoiTop(n-1, middle, start, finish, answer);
-
-}
-
-}
+    else{
+      hanoiTop(n-1, start, finish, middle, answer);
+      answer[index++] = new int[] {start, finish};
+      hanoiTop(n-1, middle, start, finish, answer);
+    }
+  }
 }
