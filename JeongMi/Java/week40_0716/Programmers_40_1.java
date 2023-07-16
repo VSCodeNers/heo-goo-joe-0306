e3935ed6d6ae3f7ddfa45e77f05948182ecc4b71
jung0115 <jungim7490@gmail.com>
2023-07-16 16:31:59 +0900

    readme

--- JeongMi/Java/week40_0716/Programmers_40_1.java
+++ JeongMi/Java/week40_0716/Programmers_40_1.java
@@ -1,21 +1,21 @@
 // 프로그래머스 - N개의 최소공배수(Lv.2)
 class Solution {
-  public int solution(int[] arr) {
-      int gcd = arr[0];
-      
-      for(int i = 1; i < arr.length; i++) {
-          // 최소공배수 = (n1 * n2) / (n1, n2의 최대공약수)
-          gcd = (gcd * arr[i]) / GCD(gcd, arr[i]);
-      }
-      
-      return gcd;
-  }
-  
-  // 최대공약수
-  static int GCD(int num1, int num2) {
-      if (num1 % num2 == 0) {
-          return num2;
-      }
-      return GCD(num2, num1%num2);
-  }
+    public int solution(int[] arr) {
+        int lcm = arr[0];
+        
+        for(int i = 1; i < arr.length; i++) {
+            // 최소공배수 = (n1 * n2) / (n1, n2의 최대공약수)
+            lcm = (lcm * arr[i]) / GCD(lcm, arr[i]);
+        }
+        
+        return lcm;
+    }
+
+    // 최대공약수
+    static int GCD(int num1, int num2) {
+        if (num1 % num2 == 0) {
+            return num2;
+        }
+        return GCD(num2, num1 % num2);
+    }
 }
