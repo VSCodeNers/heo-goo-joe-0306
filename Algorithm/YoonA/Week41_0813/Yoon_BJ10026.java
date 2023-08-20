47416da297b44359ac97bf1d0a4f37495da245bb
yoona1110 <gjdbsdk18@icloud.com>
2023-08-20 15:29:39 +0900

    41주차 자바 완

--- Algorithm/YoonA/Week41_0813/Yoon_BJ10026.java
+++ Algorithm/YoonA/Week41_0813/Yoon_BJ10026.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Yoon_BJ10026 {
     static int N;
     static char grid[][];
-    static boolean notCheck[][];
     static boolean check[][];
     static int mapX[] = {0, -1, 1, 0};
     static int mapY[] = {-1, 0, 0, 1};
@@ -17,7 +15,6 @@ public class Yoon_BJ10026 {
 
         grid = new char[N][N];
         check = new boolean[N][N];
-        notCheck = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
@@ -27,46 +24,38 @@ public class Yoon_BJ10026 {
         }
 
         // 색약이 아닌 경우
-        int colorWeakness = 0;
         int notColorWeakness = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!check[i][j]) {
                     DFS(i, j);
                     notColorWeakness++;
-                    System.out.println(notColorWeakness);
                 }
-                if (!notCheck[i][j]) {
-                    notDFS(i, j);
+            }
+        }
+        
+        // 색약인 경우
+        // 색이 초록색인 애들은 다 빨간색으로 바꿔줌 (G -> R)
+        int colorWeakness = 0;
+        check = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == 'G') {
+                    grid[i][j] = 'R';
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!check[i][j]) {
+                    DFS(i, j);
                     colorWeakness++;
-                    System.out.println(colorWeakness);
                 }
             }
         }
+
         System.out.println(notColorWeakness + " " + colorWeakness);
-    }
-
-    public static void notDFS(int x, int y) {
-        notCheck[x][y] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int xx = x + mapX[i];
-            int yy = y + mapY[i];
-
-            // 범위를 벗어나지 않은 경우
-            if(xx >= 0 && yy >= 0 && xx < N && yy < N) {
-                if (!notCheck[xx][yy]) {
-                    if ((grid[x][y] == 'R' || grid[x][y] == 'G') && (grid[xx][yy] == 'R' || grid[xx][yy] == 'G')) {
-                        notCheck[xx][yy] = true;
-                        DFS(xx, yy);
-                    }
-                    if (grid[x][y] == 'B' && grid[xx][yy] == 'B') {
-                        notCheck[xx][yy] = true;
-                        DFS(xx, yy);
-                    }
-                }
-            }
-        }
     }
 
     public static void DFS(int x, int y) {
