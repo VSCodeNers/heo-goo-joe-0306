98a5ef5a538f00561286fe9f6a5caa7ec5d9494e
jung0115 <jungim7490@gmail.com>
2023-08-23 02:27:09 +0900

    [정미] - 42주차 완

--- CodingTest/JeongMi/Week42_0820/Baekjoon_14395.java
+++ CodingTest/JeongMi/Week42_0820/Baekjoon_14395.java
@@ -3,9 +3,70 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Baekjoon_14395 {
+  static class Node{
+		long num;
+		String cal; // 연산식
+		Node(long num, String cal){
+			this.num = num;
+			this.cal = cal;
+		}
+	}
+	
+	static final long limit = 1000000000L;
   public static void main(String[] args) throws IOException {
-      
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    long s = Integer.parseInt(st.nextToken());
+    long t = Integer.parseInt(st.nextToken());
+
+    if(s == t) {
+      System.out.print(0);
+      return;
+    }
+
+    Queue<Node> queue = new LinkedList<>();
+    HashSet<Long> check = new HashSet<Long>(); // 이미 이전에 도달한 값인지 확인  
+
+    queue.add(new Node(s, ""));
+    check.add(s);
+
+    while(!queue.isEmpty()) {
+      Node pre = queue.poll();
+      long num = pre.num;
+      String cal = pre.cal;
+
+      if(num == t) {
+        System.out.print(cal);
+        return;
+      }
+
+      // *
+      if(num != 0 && num*num <= limit && !check.contains(num*num)) {
+        queue.add(new Node(num*num, cal + "*"));
+        check.add(num*num);
+      }
+      // +
+      if(num >= 0 && num+num <= limit && !check.contains(num+num)) {
+        queue.add(new Node(num+num, cal + "+"));
+        check.add(num+num);
+      }
+      // -
+      if(!check.contains(0L)) {
+        queue.add(new Node(0, cal + "-"));
+        check.add(0L);
+      }
+      // /
+      if(num != 0 && !check.contains(1L)) {
+        queue.add(new Node(1, cal + "/"));
+        check.add(1L);
+      }
+    }
+
+    System.out.print(-1);
   }
 }
--- CodingTest/JeongMi/Week42_0820/Baekjoon_1931.java
+++ CodingTest/JeongMi/Week42_0820/Baekjoon_1931.java
@@ -3,43 +3,38 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Baekjoon_1931 {
-  static int[][] meet;
-  static int N;
-  static int start = 2147483647, finish = 0;
-  static int max = 0;
 	public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
-    N = Integer.parseInt(br.readLine());
-    meet = new int[N][2];
+    int N = Integer.parseInt(br.readLine());
+    int[][] meet = new int[N][2];
     
     for(int i = 0; i < N; i++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
       meet[i][0] = Integer.parseInt(st.nextToken());
       meet[i][1] = Integer.parseInt(st.nextToken());
-      
-      if(meet[i][0] < start) start = meet[i][0];
-      if(meet[i][0] > finish) finish = meet[i][0];
     }
-    
-    checkMax(start, 0);
-    
-    System.out.print(max);
-  }
-    
-  static void checkMax(int time, int count) {
-    if(time > finish) {
-      if(count > max) max = count;
-      System.out.println(max);
-      return;
-    }
-    
+
+    // 종료시간 기준으로 오름차순 정렬
+		Arrays.sort(meet, (o1, o2) -> {
+      // 종료 시간이 같으면 시작 시간 빠른순,
+      // 종료 시간이 다르면 종료 시간 빠른순
+			return o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1];
+		});
+
+    int count = 0;   // 회의 수
+    int endTime = 0; // 직전 회의 종료 시간
     for(int i = 0; i < N; i++) {
-      if(time >= meet[i][0]) {
-        checkMax(meet[i][1], count + 1);
+      // 직전 회의 종료시간 이후에 시작하는 회의인 경우
+      if(endTime <= meet[i][0]) {
+        endTime = meet[i][1];
+        count++;
       }
     }
-  } 
+
+    System.out.print(count);
+  }
 }
