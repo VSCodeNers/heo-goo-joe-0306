b9b9932a30c3ff74df28aecbcdcf76909b68c24d
jung0115 <jungim7490@gmail.com>
2023-09-25 21:13:23 +0900

    week 47 clear

--- CodingTest/JeongMi/Week47_0924/Baekjoon_1783.java
+++ CodingTest/JeongMi/Week47_0924/Baekjoon_1783.java
@@ -1,4 +1,4 @@
-// 백준 - 병든 나이트(1738)
+// 백준 - 병든 나이트(1783)
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -12,24 +12,28 @@ public class Baekjoon_1783 {
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
 
-    int count;
+    int count = 0;
 
     // 세로 반경의 이동 가능 횟수가 0
     if(N == 1) {
       count = 1;
     }
     // 세로 반경의 이동 가능 횟수가 2번 이하
-    // 가로 반경의 이동 가능 횟수가 2번 이하
-    if(M < 4 || N < 3) {
-      count = Math.min(N, M);
+    else if(N == 2) {
+      // 세로만 따지만 3번 이동이 최대 = 4칸 방문
+      // 가로 반경에서 더 작게 나올 수 있음 = (M+1) / 2)칸 방문
+      count = Math.min(4, (M + 1) / 2);
     }
-    // 가로 반경의 이동 가능 횟수가 4번 미만
-    else if(M < 10) {
-      count = 4;
-    }
-    // 가로 반경의 이동 가능 횟수가 4번 이상
-    else {
-      count = M - 2;
+    // 세로 반경의 이동 가능 횟수가 4번 이상
+    else if(N >= 3) {
+      // 가로 반경의 이동 가능 횟수가 4번 미만
+      if(M < 7) {
+        count = Math.min(4, M);
+      }
+      // 가로 반경의 이동 가능 횟수가 4번 이상
+      else {
+        count = M - 2;
+      }
     }
 
     System.out.print(count);
